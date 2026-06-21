c035fda995a38690f7801a3b582df8212e68af99
thekamfamtx <thekamfamtx@gmail.com>
2026-06-21 08:18:51 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+const CONFIG = {
+
+/* 📲 SOCIAL LINKS */
+social: {
+  facebook: "http://facebook.com/share/1ApkbDVWTA",
+  instagram: "http://instagram.com/thekamfamtx",
+  youtube: "https://youtube.com/@TheKAMFamTX?sub_confirmation=1",
+  klub: "https://www.youtube.com/channel/UCPqBUO8PkVkOkoLjiV9rwsw/join",
+  merch: "https://the-kam-fam-shop.fourthwall.com/"
+},
+
+/* 🎬 WEEKLY VIDEOS (2 SLOT SYSTEM) */
+videos: [
+  {
+    title: "Carnival Dream SEA Cruise Day 1 Part 1",
+    time: "Tuesday 7:00 PM CST",
+    thumb: "images/video1.jpg",
+    link: "https://youtube.com/watch?v=VIDEO1"
+  },
+  {
+    title: "WHY WOULDA WEDNESDAYS | EPISODE 3",
+    time: "Wednesday 12:34 PM",
+    thumb: "images/video2.jpg",
+    link: "https://youtube.com/watch?v=VIDEO2"
+  }
+]
+
+};
