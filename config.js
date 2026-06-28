a18427dc93fb952fca7195df45348a2f74119fed
thekamfamtx <thekamfamtx@gmail.com>
2026-06-28 14:12:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -12,12 +12,12 @@ social: {
 /* 🎬 WEEKLY VIDEOS */
 videos: [
   {
-    title: "Carnival Dream SEA Cruise Day 1 Part 1",
-    time: "Tuesday 7:00 PM CST",
+    title: "Carnival Dream Day 2 | Super Bowl Sunday at Sea, Bagels & Silent Disco!",
+    time: "Tuesday 4:00 PM CST",
     thumb: "images/video1.jpg"
   },
   {
-    title: "WHY WOULDA WEDNESDAYS | EPISODE 3",
+    title: "WHY WOULDA WEDNESDAYS | EPISODE 4",
     time: "Wednesday 12:34 PM",
     thumb: "images/video2.jpg"
   }
